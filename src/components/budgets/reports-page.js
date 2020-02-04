da299da7020543cb89836bcbd534843e476a9760
Clayton Orman <claytonkfc@Claytons-MacBook-Pro.local>
2020-02-03 23:25:22 -0500

    :construction: add initial page for reporting

---
+++ src/components/budgets/reports-page.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+// Redux
+import { loadBudgets } from "../../redux/actions/budget-actions";
+import { loadCategories } from "../../redux/actions/category-actions";
+
+const ReportsPage = ({ budgetsByYear, loadBudgets, loadCategories }) => {
+  useEffect(() => {
+    loadBudgets().catch(error => {
+      console.log(`Loading budgets failed ${error}`);
+    });
+    loadCategories().catch(error => {
+      console.log(`Loading categories failed ${error}`);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2>Reports</h2>
+      <ul>
+        {budgetsByYear.map(budget => {
+          return <li key={budget._id}>{budget.title}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+ReportsPage.propTypes = {
+  budgetsByYear: PropTypes.array.isRequired,
+  categoriesByYear: PropTypes.array.isRequired,
+  loadBudgets: PropTypes.func.isRequired,
+  loadCategories: PropTypes.func.isRequired
+};
+
+const mapStateToProps = ({ budgets, categories }) => {
+  return {
+    budgetsByYear: categories.length === 0 ? [] : budgets,
+    categoriesByYear: categories
+  };
+};
+
+const mapDispatchToProps = {
+  loadBudgets,
+  loadCategories
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReportsPage);
